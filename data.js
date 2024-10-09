fa2ec1ec68915a975e6ba78493cfc055de91962f
Sjoerd Vermeijden <sjoerd_v92@live.nl>
2024-10-09 14:48:15 +0200

    Adds data for the jobs

--- data.js
+++ data.js
@@ -3,30 +3,50 @@ export const jobs = [
         id: 0,
         title: 'Front-end developer',
         description: '',
-        salary: '100k'
+        salary: '100k',
+        placed: '22-01-1992',
+        postal_code: '3133AT',
+        vacancy_language: 'English',
+        contract_type: 'Full-time'
     },
     {
         id: 1,
         title: 'Front-end developer',
         description: '',
-        salary: '100k'
+        salary: '100k',
+        placed: '22-01-1992',
+        postal_code: '3133AT',
+        vacancy_language: 'English',
+        contract_type: 'Full-time'
     },
     {
         id: 2,
         title: 'Front-end developer',
         description: '',
-        salary: '100k'
+        salary: '100k',
+        placed: '22-01-1992',
+        postal_code: '3133AT',
+        vacancy_language: 'English',
+        contract_type: 'Full-time'
     },
     {
         id: 3,
         title: 'Front-end developer',
         description: '',
-        salary: '100k'
+        salary: '100k',
+        placed: '22-01-1992',
+        postal_code: '3133AT',
+        vacancy_language: 'Dutch',
+        contract_type: 'Full-time'
     },
     {
         id: 4,
         title: 'Front-end developer',
         description: '',
-        salary: '100k'
+        salary: '100k',
+        placed: '22-01-1992',
+        postal_code: '3133AT',
+        vacancy_language: 'English',
+        contract_type: 'Full-time'
     },
 ]
