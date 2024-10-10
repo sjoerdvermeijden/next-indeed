21d6bac5e30ae9207650337583f3d28ce68d3efd
Sjoerd Vermeijden <sjoerd_v92@live.nl>
2024-10-10 11:13:32 +0200

    Adds more styling yet again to the highlighted vacancy

--- src/app/components/layout/HighlightedJob.tsx
+++ src/app/components/layout/HighlightedJob.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { IconBriefcase, IconClockFilled, IconCoins } from '@tabler/icons-react';
+import { IconBriefcase, IconClockFilled, IconCoins, IconLink } from '@tabler/icons-react';
 
 import Image from 'next/image'
 
@@ -20,7 +20,9 @@ function HighlightedJob({ title, company, background_image, postal_code }: Job) 
             </div>
             <div className='p-4 shadow-md'>
                 <h3 className='text-2xl font-bold mb-4'>{title}</h3>
-                <a href="https://www.google.nl/" className='underline' target="_blank">{company}</a>
+                <div className='flex mb-1'>
+                    <a href="https://www.google.nl/" className='underline mr-2' target="_blank">{company}</a><IconLink size={20} />
+                </div>
                 <p>{postal_code}</p>
             </div>
             <div className='border-b-1 p-4'>
