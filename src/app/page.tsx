7a99067bc8f3a26080af8321dc0863a8d8892946
Sjoerd Vermeijden <sjoerd_v92@live.nl>
2024-10-22 08:56:08 +0200

    Removes all from the search context

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,28 +4,19 @@ import Jobs from './components/layout/Jobs'
 
 import { joblist } from '../../data'
 
-import React, { useEffect, useState, useContext } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { Job } from './types/job'
 
 import Wrap from './components/layout/Wrap'
 import SearchForm from './components/layout/SearchForm'
 
-import { SearchContext } from './context/searchContext'
-
 import { Tabs, Tab } from "@nextui-org/tabs";
 
 import HighlightedJob from './components/layout/HighlightedJob';
 
-type SearchItem = {
-  id: number,
-  title: string
-}
-
 export default function Home({ }) {
   const [vacancy, setVacancy] = useState<Job>();
-
-  const [searchItems] = useContext(SearchContext);
 
   useEffect(() => {
     const result = joblist.filter((job) => Number(job.id) === 1);
@@ -47,11 +38,7 @@ export default function Home({ }) {
             <Tab key="search-results" title="Recente zoekopdrachten">
               <div className='flex justify-center gap-5 relative grow'>
                 <ul>
-                  {
-                    searchItems.map((item: SearchItem) => {
-                      return <li key={item.id}>{item.title}</li>
-                    })
-                  }
+                  <li>Test</li>
                 </ul>
               </div>
             </Tab>
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,17 +2,15 @@
 
 import React from 'react'
 
-import { SearchComponent } from './context/searchContext'
-
 type Props = {
     children: React.ReactNode
 }
 
 function Providers({ children }: Props) {
     return (
-        <SearchComponent>
+        <>
             {children}
-        </SearchComponent>
+        </>
     )
 }
 
