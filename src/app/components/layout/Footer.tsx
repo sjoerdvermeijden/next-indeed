95567d852bf0675aa6ef546b3d100391246dfed5
Sjoerd Vermeijden <sjoerd_v92@live.nl>
2025-09-08 16:41:32 +0200

    Adds padding for the footer

--- src/app/components/layout/Footer.tsx
+++ src/app/components/layout/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
     return (
-        <div className='bg-gray-700 py-2 px-4 text-white'>Footer</div>
+        <div className='bg-gray-700 py-2 px-6 text-white'>Footer</div>
     )
 }
 
