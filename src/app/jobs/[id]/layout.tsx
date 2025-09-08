a5d595933208c8d90670e4202e506cdf3a86a6bd
Sjoerd Vermeijden <sjoerd_v92@live.nl>
2025-09-08 10:49:48 +0200

    Removes the mobile conditional

--- src/app/jobs/[id]/layout.tsx
+++ src/app/jobs/[id]/layout.tsx
@@ -2,8 +2,6 @@
 
 import React from 'react'
 
-import useWindowWidth from "../../utils/useWindowWidth";
-
 import Wrap from '../../components/layout/Wrap';
 import Jobs from '../../components/layout/Jobs';
 
@@ -12,24 +10,13 @@ type Props = {
 }
 
 export default function Layout({ children }: Props) {
-    const width = useWindowWidth();
 
     return (
         <>
             <Wrap>
                 <div className='relative w-full py-12 sm:flex sm:justify-center sm:gap-7 sm:grow'>
-
-                    {
-                        width < 1024 ?
-                        (
-                            children
-                        )
-                        :
-                        <>
-                            <Jobs />
-                            {children}
-                        </>
-                    }
+                    <Jobs />
+                    {children}
                 </div>
             </Wrap>
         </>
